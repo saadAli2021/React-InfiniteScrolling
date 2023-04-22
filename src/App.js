2f61aa493dcadf21f7c65d782b85727af14e627d
saadali2021 <saadfiver4@gmail.com>
2023-04-23 00:47:15 +0200

    infinite scroll implemented (basic)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Photo from "./Photo";
 
 const clientID = `?client_id=${process.env.REACT_APP_APIKEY}`;
-const url = "https://api.unsplash.com/photos/";
+const pageID = `&page=`;
+const mainurl = `https://api.unsplash.com/photos/${clientID}`;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchImages = async () => {
+    let url = `${mainurl}${pageID}${page}`;
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setPhotos((oldPhotos) => [...oldPhotos, ...data]);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      const innerHeight = window.innerHeight;
+      const scrollAmount = window.scrollY;
+      const bodyHeight = document.body.scrollHeight;
+      const offset = -2;
+
+      if (innerHeight + scrollAmount >= bodyHeight + offset) {
+        setPage((oldpage) => oldpage + 1);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", event);
+    };
+  }, []);
+
   return (
     <div className="App">
       <main className="MainSection">
@@ -13,11 +54,9 @@ function App() {
           <button className="searchBtn">Search</button>
         </article>
         <article className="photos">
-          <Photo />
-          <Photo />
-          <Photo />
-          <Photo />
-          <Photo />
+          {photos.map((photo, index) => {
+            return <Photo key={index} {...photo} />;
+          })}
         </article>
       </main>
     </div>
--- src/Photo.js
+++ src/Photo.js
@@ -1,14 +1,10 @@
 import React from "react";
 
-const Photo = () => {
+const Photo = ({ urls: { regular }, alt_description }) => {
   return (
     <div className="imageContainer">
-      <img
-        src="https://images.unsplash.com/photo-1555169062-013468b47731?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmlyZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        alt="asd"
-        className="img"
-      />
-      <h3>name of image</h3>
+      <img src={regular} alt="asd" className="img" />
+      <p>{alt_description}</p>
     </div>
   );
 };
